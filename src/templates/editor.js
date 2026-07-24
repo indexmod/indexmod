f0ea4e9c32447a4b947d114f75388b6cd791f813
indexmod <23720911+indexmod@users.noreply.github.com>
2026-07-24 23:30:06 +0300

    Update editor.js

--- src/templates/editor.js
+++ src/templates/editor.js
@@ -1,21 +1,82 @@
-export default function editorTemplate(){
+export default function editorTemplate(page = {}) {
 
 return `
 
-<h1>New page</h1>
+<h1>
+${page.slug ? "Edit" : "New"}
+</h1>
 
-<textarea id="md">
----
-title:
-slug:
----
 
-Write here...
-</textarea>
+<textarea id="md">${page.content || ""}</textarea>
 
-<button>
+
+<button onclick="save()">
 Save
 </button>
+
+
+
+<script>
+
+
+async function save(){
+
+
+const md =
+document.getElementById("md").value;
+
+
+
+const slug =
+
+(
+md.match(
+/slug:\s*(.*)/
+)?.[1]
+|| "untitled"
+)
+
+.trim()
+
+.toLowerCase()
+
+.replace(
+/[^a-z0-9-]/g,
+"-"
+);
+
+
+
+await fetch("/_save", {
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:
+JSON.stringify({
+
+slug,
+
+content:md
+
+})
+
+});
+
+
+
+location.href =
+"/" + slug;
+
+
+}
+
+
+</script>
+
 
 `;
 
