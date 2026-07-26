c1b7dd4e899074b81ede164f3a86b04b99bf5ca4
indexmod <23720911+indexmod@users.noreply.github.com>
2026-07-26 12:19:06 +0300

    Update layout.js

--- src/templates/layout.js
+++ src/templates/layout.js
@@ -66,7 +66,7 @@ content="${escapeHtml(description)}">
 
 
 <link rel="canonical"
-href="${url}">
+href="${escapeHtml(url)}">
 
 
 
@@ -84,13 +84,15 @@ url
 
 
 
-<link rel="stylesheet" href="/styles/base.css">
+<link rel="stylesheet" href="/styles/1/base.css">
 
-<link rel="stylesheet" href="/styles/view.css">
+<link rel="stylesheet" href="/styles/1/view.css">
 
-<link rel="stylesheet" href="/styles/editor.css">
+<link rel="stylesheet" href="/styles/1/editor.css">
 
-<link rel="stylesheet" href="/styles/index.css">
+<link rel="stylesheet" href="/styles/1/index.css">
+
+
 
 ${yandexMetrika()}
 
